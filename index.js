bae26e726f3811d212049e0e2f3ee2fa31dd5a24
daviddevops2000 <david.devops2000@gmail.com>
2024-01-10 12:59:28 -0300

    set visit count to zero

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const redis = require('redis');
 const app = express();
 const client = redis.createClient();
 
+client.set('visits', 0);
+
 app.get('/', (req, res)=>{
     client.get('visits', (err, visits)=>{
         res.send(`Number of visits ${visits}`);
